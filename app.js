0fad15518efbf702ed8ab789651d673f43313de5
codingxllama <osamaalchalabi2@gmail.com>
2019-07-30 21:51:07 -0600

    Made the basic html,css,js file

---
+++ app.js
@@ -0,0 +1,29 @@
+// Creating the required elements
+const text= ['Developer','Problem Solver','Student'];
+let counter=0;
+let index=0;
+let currentText = '';
+let letters ='';
+
+
+(function typer()
+{
+if(counter === text.length)
+{
+    counter= 0;
+}
+currentText = text[counter];
+letters= currentText.slice(0, ++index);
+
+
+document.querySelector('.typing').textContent=letters;
+if(letters.length===currentText.length)
+{
+    counter++;
+    index=0;
+}
+
+setTimeout(typer,200);
+
+
+})();
